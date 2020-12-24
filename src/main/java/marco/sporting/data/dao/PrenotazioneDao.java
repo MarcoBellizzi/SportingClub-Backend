a73ed6f598f9396f5d7fa99ad94b57743f6e81a2
Marco Bellizzi <marcobellizzi96@gmail.com>
2020-12-24 10:49:52 +0100

    refactored

--- src/main/java/marco/sporting/data/dao/PrenotazioneDao.java
+++
@@ -1,9 +0,0 @@
-package marco.sporting.data.dao;
-
-import marco.sporting.data.entity.Prenotazione;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrenotazioneDao extends JpaRepository<Prenotazione, Long> {
-}
